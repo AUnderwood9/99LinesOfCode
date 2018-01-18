8ed283c67124a292a4b8cf1424af83b2b070a058
AUnderwood9 <aunderwood797@gmail.com>
2018-01-18 17:30:23 -0600

    Displayed the song lyrics and the names of the friends who are singing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myFriends = ["Tyrion", "Tyler Durden", "Dragonborn", "Joshua Graham", "Jon Snow"];
-let divFriend = document.getElementById("friend");
+let singingButton = document.getElementById("sing");
 let lyricArray = [];
+
 let createElement = function(nodeToAppend, elementType, contentToAppend) {
     let nodeToAdd = document.createElement(elementType);
     let nodeText = document.createTextNode(contentToAppend);
@@ -9,15 +10,15 @@ let createElement = function(nodeToAppend, elementType, contentToAppend) {
     nodeToAppend.appendChild(nodeToAdd);
 }
 
-document.addEventListener('DOMContentLoaded', function (){
+let createNodeAndClass = function(elementType, contentToAppend, elementClass) {
+    let nodeToAdd = document.createElement(elementType);
+    let nodeHeader = document.createElement("h3");
+    let nodeText = document.createTextNode(contentToAppend);
+    nodeToAdd.className = elementClass;
+    nodeHeader.appendChild(nodeText);
+    nodeToAdd.appendChild(nodeHeader);
 
-    // manipulate elements
-    console.log("dom content loaded");
-    
-    });
-
-while (divFriend.hasChildNodes()) {
-    divFriend.removeChild(divFriend.lastChild);
+    document.body.appendChild(nodeToAdd);
 }
 
 myFriends.forEach(function(item){
@@ -40,9 +41,36 @@ myFriends.forEach(function(item){
     });
 });
 
-for(item in lyricArray){
-    createElement(divFriend, "p", lyricArray[item]["friendName"]);
-    for(item2 in lyricArray[item]["friendLyric"]){
-        createElement(divFriend, "p", lyricArray[item]["friendLyric"][item2]);
-    }
-}
+
+document.addEventListener('DOMContentLoaded', function (){
+
+    // manipulate elements
+    singingButton.addEventListener("click", () => {
+        //console.log("You clicked me");
+        /*
+        myFriends.forEach(function(item){
+            console.log(`${item.toUpperCase()}`);
+            for(i = 99; i > 0; i--){
+                if(i > 2){
+                    console.log(`${i} lines of code in the file, ${i} lines of code; ${item} strikes one out, clears it all out, ${i -1} lines of code in the file`);
+                }else if(i === 1){
+                    console.log(`${i} line of code in the file, ${i} line of code; ${item} strikes one out, clears it all out, no more lines of code in the file`);
+                }else{
+                    console.log(`${i} lines of code in the file, ${i} lines of code; ${item} strikes one out, clears it all out, ${i -1} line of code in the file`);
+                }
+            }
+        });
+        */
+
+        for(item in lyricArray){
+            createNodeAndClass("div", lyricArray[item]["friendName"], "friend");
+            let divFriendNames = document.getElementsByClassName("friend");
+            //createElement(divFriendNames[item], "p", "stuff");
+            for(item2 in lyricArray[item]["friendLyric"]){
+                createElement(divFriendNames[item], "p", lyricArray[item]["friendLyric"][item2]);
+
+            }
+        }
+    });
+
+});
