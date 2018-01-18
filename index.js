dde5e33fa060290de0c03f70e04710bf7bd7556c
AUnderwood9 <aunderwood797@gmail.com>
2018-01-18 13:17:58 -0600

    Output song lines to index.html

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 let myFriends = ["Tyrion", "Tyler Durden", "Dragonborn", "Joshua Graham", "Jon Snow"];
+let divFriend = document.getElementById("friend");
+let lyricArray = [];
+let createElement = function(nodeToAppend, elementType, contentToAppend) {
+    let nodeToAdd = document.createElement(elementType);
+    let nodeText = document.createTextNode(contentToAppend);
+    nodeToAdd.appendChild(nodeText);
+
+    nodeToAppend.appendChild(nodeToAdd);
+}
+
+while (divFriend.hasChildNodes()) {
+    divFriend.removeChild(divFriend.lastChild);
+}
 
 myFriends.forEach(function(item){
-    let upperCased = item.toUpperCase();
-    console.log(`${upperCased}:`);
+    let lyricLine = "";
+    let lyricLineArray = [];
     for(i = 99; i > 0; i--){
         if(i > 1){
-            console.log(`${i} lines of code in the file, ${i} lines of code; ${item} strikes one out, clears it all out, ${i -1} lines of code in the file}`);
+            lyricLine = `${i} lines of code in the file, ${i} lines of code; ${item} strikes one out, clears it all out, ${i -1} lines of code in the file`;
         }else{
-            console.log(`${i} line of code in the file, ${i} line of code; ${item} strikes one out, clears it all out, no more lines of code in the file`);
+            lyricLine = `${i} line of code in the file, ${i} line of code; ${item} strikes one out, clears it all out, no more lines of code in the file`;
         }
+        lyricLineArray.push(lyricLine);
     }
+
+    lyricArray.push({
+        friendName: item,
+        friendLyric: lyricLineArray
+    });
 });
+
+for(item in lyricArray){
+    createElement(divFriend, "p", lyricArray[item]["friendName"]);
+    for(item2 in lyricArray[item]["friendLyric"]){
+        createElement(divFriend, "p", lyricArray[item]["friendLyric"][item2]);
+    }
+}
